7008ca27744e1942bf8a4f2f700f735c2bb4f756
moryahng <92284820+moryahng@users.noreply.github.com>
2025-05-23 18:26:56 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-var heroheight = document.getElementById('herobanner').clientHeight;
+var heroheight = document.getElementById("herobanner").clientHeight;
 document.getElementById("aboutus").style.marginTop = heroheight + "px";
 
-window.addEventListener('resize', function(event) {
-    var heroheight = document.getElementById('herobanner').clientHeight;
+window.addEventListener(
+  "resize",
+  function (event) {
+    var heroheight = document.getElementById("herobanner").clientHeight;
     document.getElementById("aboutus").style.marginTop = heroheight + "px";
-}, true);
+  },
+  true
+);
 
 function scrollToBuyACask() {
-    const isMobile = window.innerWidth <= 900;
-    const targetId = isMobile ? 'anchor_buyacask_mobile' : 'anchor_buyacask_desktop';
-    const target = document.getElementById(targetId);
-    if (target) {
-        target.scrollIntoView({ behavior: 'smooth' });
-    }
+  const isMobile = window.innerWidth <= 900;
+  const targetId = isMobile
+    ? "anchor_buyacask_mobile"
+    : "anchor_buyacask_desktop";
+  const target = document.getElementById(targetId);
+  if (target) {
+    target.scrollIntoView({ behavior: "smooth" });
+  }
 }
 
 //
@@ -24,5 +30,5 @@ function scrollToBuyACask() {
 //    }
 //else{
 //    document.getElementById("navbar_white").style.display = "block";
-//    document.getElementById("navbar").style.display = "none";  
+//    document.getElementById("navbar").style.display = "none";
 //}}
